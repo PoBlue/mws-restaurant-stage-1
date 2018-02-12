1021ae63cd481f1b7fdaf681a80703f2685511ef
PoBlue <250299430@qq.com>
2018-02-12 14:27:42 +0800

    [Accessibility] set alt to img

--- js/main.js
+++ js/main.js
@@ -140,6 +140,7 @@ createRestaurantHTML = (restaurant) => {
 
   const image = document.createElement('img');
   image.className = 'restaurant-img';
+  image.setAttribute('alt', restaurant.name); //set alt to img tag
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
   li.append(image);
 

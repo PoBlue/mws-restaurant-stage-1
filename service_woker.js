63fdecf90282fa551ff38a479240ea36ae1bdb98
PoBlue <250299430@qq.com>
2018-03-27 14:04:02 +0800

    cache images and index.html

--- service_woker.js
+++ service_woker.js
@@ -5,6 +5,17 @@ this.addEventListener('install', function (event) {
         caches.open(staticCacheName).then(function (cache) {
             return cache.addAll([
                 '/',
+                'index.html',
+                'img/1.jpg',
+                'img/2.jpg',
+                'img/3.jpg',
+                'img/4.jpg',
+                'img/5.jpg',
+                'img/6.jpg',
+                'img/7.jpg',
+                'img/8.jpg',
+                'img/9.jpg',
+                'img/10.jpg',
                 'css/styles.css',
                 '/restaurant.html',
                 'data/restaurants.json',

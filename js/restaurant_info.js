767978a1a48f986f125e07030bc4c14a88e75b29
PoBlue <250299430@qq.com>
2018-03-27 13:59:03 +0800

    [imporve] readme & alt for image

--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -59,6 +59,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
 
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
+  image.setAttribute('alt', `An image of ${restaurant.name}`); //set alt to img tag
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
 
   const cuisine = document.getElementById('restaurant-cuisine');
